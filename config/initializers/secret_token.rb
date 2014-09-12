9cd4eb14951ebc68c02f07a074da08df501dfb01
nicolehunnicutt <nicolehunnicutt@students.abtech.edu>
2014-09-11 20:36:50 -0400

    Chapter 3 first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chap3::Application.config.secret_token = '72727b540789101a6eb77afd46bf8cd4a3df4d06476723194eb77317c33466250f3effac5a0fb6a221bea1c167a63709361b9b924a9458d025fe90098d0db03e'
